1c88ce910dc7dce3fa12fa810a8da4ccfee69f02
Pavel Zhdanovich <zhdan21@gmail.com>
2021-12-30 11:08:50 +0300

    добавлен redux-presisted

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,18 @@ import 'reset-css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import App from './App';
 import store from './store/store';
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root') || document.querySelectorAll('body')[0],
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, createStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
 import { paginationSlice } from './pockemonSlice';
 import { pockemonPageSlice } from './pockeonPageSlice';
 
-const store = configureStore({
-  reducer: {
-    pagination: paginationSlice.reducer,
-    pockemonPage: pockemonPageSlice.reducer,
-  },
+const rootReducer = combineReducers({
+  pagination: paginationSlice.reducer,
+  pockemonPage: pockemonPageSlice.reducer,
 });
+
+export const config = {
+  key: 'root',
+  storage,
+};
+
+const persisted = persistReducer(config, rootReducer);
+
+const store = createStore(persisted);
 
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
